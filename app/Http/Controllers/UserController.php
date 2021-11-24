0ba2497a04f1185c88814a05d4866a83a0ee3065
daw20-06 <nada>
2021-11-24 15:50:59 +0100

    filtro por nombre y por equipo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,9 +16,18 @@ class UserController extends Controller
     public function index()
     {
         $users = User::query()
+            ->when(request('team'), function ($query, $team){
+                if ($team === 'with_team') {
+                    $query->has('team');
+                }elseif ($team === 'without_team'){
+                    $query->doesntHave('team');
+                }
+            })
             ->when(request('search'), function ($query, $search){
-                $query->where('name', 'LIKE', "%{$search}%")
-                ->orWhere('email', 'LIKE', "%{$search}%"); //Esto es un Or para las consultas
+                $query->where(function ($query) use ($search){
+                    $query->where('name', 'LIKE', "%{$search}%")
+                        ->orWhere('email', 'LIKE', "%{$search}%");
+                });
             })
             ->orderBy('created_at', 'DESC')
             ->paginate();
